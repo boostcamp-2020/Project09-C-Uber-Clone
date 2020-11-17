8620d927752dcf6c290122f728c36f7d85ca9503
Heonjun LEE <dklee730@gmail.com>
2020-11-17 20:28:58 +0900

    feat: client 개발 환경 설정

---
+++ client/.eslintrc.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  settings: {
+    react: {
+      'version': 'detect',
+    },
+  },
+  rules: {
+    indent: ['error', 2, { 'SwitchCase': 1 }],
+    'semi': ['error', 'always'],
+    'no-trailing-spaces': 'error',
+    curly: 'error',
+    'brace-style': 'error',
+    'no-multi-spaces': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': 'error',
+    'no-whitespace-before-property': 'error',
+    'func-call-spacing': 'error',
+    'space-before-blocks': 'error',
+    'keyword-spacing': ['error', { before: true, after: true }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-in-parens': ['error', 'never'],
+    'block-spacing': 'error',
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'key-spacing': ['error', { mode: 'strict' }],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'eol-last': 'error',
+    quotes: ['error', 'single'],
+    'react/prop-types': 'off',
+  },
+};
---
+++ client/webpack.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.jsx',
+  },
+  output: {
+    filename: 'bundle.js',
+    publicPath: '/',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: 'babel-loader',
+      exclude: /node_modules/,
+    }, {
+      test: /\.css$/,
+      use: ['style-loader'],
+      exclude: /node_modules/,
+    }],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    overlay: true,
+    stats: 'errors-only',
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })],
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
