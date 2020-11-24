2454128549862f2b74667ddfedca741309d03016
passwd10 <parkinseo94@naver.com>
2020-11-24 15:25:15 +0900

    feat: React-Router 구현
    - browser router사용

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,18 @@ import 'antd-mobile/dist/antd-mobile.css';
 
 import React from 'react';
 
+import { Route, Switch } from 'react-router-dom';
+
 import LoginPage from './pages/LoginPage';
+import SignUpSelectPage from './pages/SignUpSelectPage';
 
 export default function App() {
   return (
     <>
-      <LoginPage />
+      <Switch>
+        <Route path='/login' component={LoginPage} />
+        <Route path='/signup/select' component={SignUpSelectPage} />
+      </Switch>
     </>
   );
 }
