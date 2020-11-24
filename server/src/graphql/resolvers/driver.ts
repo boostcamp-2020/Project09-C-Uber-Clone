8f13b487d0c2629f5daa47ebd579d5fdae7f9512
kjk <kkole3897@naver.com>
2020-11-25 02:37:19 +0900

    feat: DriverResolver 추가
    - Driver 관련 graphql이 어떤 식으로 데이터를 가져올지 정의
    - db에서 email로 driver 정보 가져오도록 정의
    - driver 정보를 받아 새로운 driver를 db에 추가하도록 정의

---
+++ server/src/graphql/resolvers/driver.ts
@@ -0,0 +1,25 @@
+import { Driver } from '../../services';
+
+interface createDriverArgs {
+  email: string;
+  name: string;
+  password: string;
+  phoneNumber: string;
+  carType: string;
+  plateNumber: string;
+  description: string;
+  profileImage: string;
+}
+
+export default {
+  Query: {
+    async driver(parent: any, args: { email: string }, context: any, info: any) {
+      return await Driver.getDriverInfo({ email: args.email });
+    },
+  },
+  Mutation: {
+    async createDriver(parent: any, args: createDriverArgs, context: any, info: any) {
+      return await Driver.signup({ ...args });
+    },
+  },
+};
