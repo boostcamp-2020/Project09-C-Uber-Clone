c11d0a650a161a299c4b9e5d3fe06e3397c142da
kjk <kkole3897@naver.com>
2020-11-25 02:55:55 +0900

    feat: 비밀번호 암호화
    bcrypt를 이용해서 비밀번호 암호화

--- server/src/services/driver.ts
+++ server/src/services/driver.ts
@@ -1,9 +1,15 @@
+import 'dotenv/config';
+import bcrypt from 'bcrypt';
 import { Driver } from '../repositories';
+
+const saltRounds = parseInt(process.env.SALT_ROUNDS || '10');
 
 export default {
   signup: async (payload) => {
+    const { password } = payload;
     try {
-      const driver = await Driver.create(payload);
+      const hash = await bcrypt.hash(password, saltRounds);
+      const driver = await Driver.create({ ...payload, password: hash });
       return driver;
     } catch (e) {
       throw e.message;
