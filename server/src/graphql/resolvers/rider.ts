d54a1476a9fec92365aac6254db8997c4a1c67ec
kjk <kkole3897@naver.com>
2020-11-25 14:51:17 +0900

    feat: createRider resolver 추가 및 rider resolver 삭제
    - schema에서 rider 삭제해서 resolver 필요 없어짐
    - createRider resolver 추가해서 회원가입 api 사용할 수 있도록 함

--- server/src/graphql/resolvers/rider.ts
+++ server/src/graphql/resolvers/rider.ts
@@ -1,17 +1,25 @@
 import { Rider } from '../../services';
 
 interface LoginPayload{
-  email:string,
-  password:string
+  email:string;
+  password:string;
+}
+
+interface createRiderArgs {
+  email: string;
+  password: string;
+  name: string;
+  phoneNumber: string;
 }
 
 export default {
-  Query: {
-    rider: () => 'Hello Rider',
-  },
   Mutation: {
     loginRider:
       async (_: any, payload:LoginPayload, context) =>
         await Rider.login(context, payload),
+    createRider:
+      async (parent: any, payload: createRiderArgs, context: any) => {
+        await Rider.signup(payload);
+      },
   },
 };
