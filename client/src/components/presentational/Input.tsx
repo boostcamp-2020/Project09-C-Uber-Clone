0c78b05a8aee98d2ff88fad7490072295d6546e6
kjk <kkole3897@naver.com>
2020-11-27 01:15:44 +0900

    fix: Input에서 value field 삭제
    - 이 컴포넌트에 의존하고 있는 다른 컴포넌트에서 입력이 제대로 안되는 문제 때문에 삭제

--- client/src/components/presentational/Input.tsx
+++ client/src/components/presentational/Input.tsx
@@ -6,17 +6,15 @@ interface InputProps {
   type: 'number' | 'text' | 'bankCard' | 'phone' | 'password' | 'digit' | 'money';
   placeholder?: string;
   onChange?: any;
-  value?: string;
 }
 
-const Input: FunctionComponent<InputProps> = ({ type, placeholder, onChange, value }) => {
+const Input: FunctionComponent<InputProps> = ({ type, placeholder, onChange }) => {
 
   return (
     <InputItem
       type={type}
       placeholder={placeholder}
       onChange={onChange}
-      value={value}
     />
   );
 };
