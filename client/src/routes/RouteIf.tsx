3a4bed831b9b429a352b652d6287cda7e9444586
Heonjun LEE <dklee730@gmail.com>
2020-11-26 17:41:00 +0900

    feat: 토큰여부를 확인하여 라우팅

---
+++ client/src/routes/RouteIf.tsx
@@ -0,0 +1,34 @@
+import React, { FunctionComponent } from 'react';
+
+import {
+  BrowserRouter as Router, Switch, Route, Redirect,
+} from 'react-router-dom';
+
+import SetCoursePage from '../pages/SetCoursePage';
+interface Paths {
+  path: string;
+}
+
+const RouteIf: FunctionComponent<Paths> = ({ path }) => {
+  console.log({ path });
+  return (
+    <Route
+      path={path}
+      render={() => {
+        if (localStorage.getItem('token')) {
+          return (
+            <Router>
+              <Switch>
+                <Route path='/setcourse' component={SetCoursePage} />
+                <Redirect path="*" to="/login" />
+              </Switch>
+            </Router>
+          );
+        }
+        return <Redirect to="/login"/>;
+      }}
+    />
+  );
+};
+
+export default RouteIf;
