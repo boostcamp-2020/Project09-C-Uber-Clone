12b12cfa6b352ab66f311d29b4689c7b636c9d88
passwd10 <parkinseo94@naver.com>
2020-11-28 16:03:15 +0900

    feat: driverSignUp 관련 api통신 함수 signUpAPI에 추가

--- client/src/apis/signUpAPI.tsx
+++ client/src/apis/signUpAPI.tsx
@@ -7,6 +7,15 @@ interface RiderInfo {
   password: string;
 }
 
+interface DriverInfo {
+  name: string;
+  phoneNumber: string;
+  email: string;
+  password: string;
+  carType: string;
+  plateNumber: string;
+}
+
 export const requestRiderSignUp = async(client: any, history: any, riderInfo: RiderInfo) => {
   try {
     const { data } = await client.mutate({
@@ -22,4 +31,17 @@ export const requestRiderSignUp = async(client: any, history: any, riderInfo: Ri
     window.alert(`회원가입 실패\n원인${error}`);
     history.push('/signup/rider') ;
   }
+};
+
+export const requestDriverSignUp = async(client: any, history: any, driverInfo: DriverInfo) => {
+  const { data } = await client.mutate({
+    mutation: signUpDriver,
+    variables: { ...driverInfo },
+    fetchPolicy: 'no-cache',
+  });
+  if (data.createRider) {
+    history.push('/login');
+  }
+  window.alert('회원가입 실패');
+  history.push('/signup/driver') ;
 };
