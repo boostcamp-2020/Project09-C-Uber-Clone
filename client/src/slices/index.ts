8096226f83e2fef34fb1e72009c00f413e1d0856
passwd10 <parkinseo94@naver.com>
2020-11-28 15:31:21 +0900

    refactor: 사용되지 않는 loginSlice파일 삭제
    - 전역이 아닌 컴포넌트 내부에서 상태관리

--- client/src/slices/index.ts
+++ client/src/slices/index.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 
-import loginReducer from './loginSlice';
 import signUpReducer from './signUpSlice';
 import mapReducer from './mapSlice';
 
 const reducer = combineReducers({
-  loginReducer,
   signUpReducer,
   mapReducer,
 });
