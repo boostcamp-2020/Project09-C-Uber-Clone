2ea0a26e12e045e1988f0ba7dbfde9561a3313f1
passwd10 <parkinseo94@naver.com>
2020-11-28 16:38:14 +0900

    feat: SubmitButton에 disabled props 추가
    - validation을 만족하지 못하면 disable

--- client/src/components/presentational/SubmitButton.tsx
+++ client/src/components/presentational/SubmitButton.tsx
@@ -5,14 +5,16 @@ import { Button } from 'antd-mobile';
 interface SubmitButtonProps {
   content: string;
   onClick: any;
+  disabled: boolean;
 }
 
-const SubmitButton: FunctionComponent<SubmitButtonProps> = ({ content, onClick }) => {
+const SubmitButton: FunctionComponent<SubmitButtonProps> = ({ content, onClick, disabled }) => {
   return (
     <Button
       type='primary'
       style={{ backgroundColor: '#56A902' }}
       onClick={onClick}
+      disabled={disabled}
     >
       {content}
     </Button>
