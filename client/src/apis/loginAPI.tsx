d7c292b24c4bf6d95e6f66a15a2a72f0fc374c1a
passwd10 <parkinseo94@naver.com>
2020-11-28 15:24:46 +0900

    feat: login시 통신만 담당하는 loginAPI 파일 생성

---
+++ client/src/apis/loginAPI.tsx
@@ -0,0 +1,18 @@
+import { loginRiderQuery, loginDriverQuery } from '../queries/login';
+
+export const requestLogin = async (client: any, history: any, riderCheck : boolean, email: string, password: string) => {
+  const { data } = await client.mutate({
+    mutation: riderCheck ? loginRiderQuery : loginDriverQuery,
+    variables: { email, password },
+    fetchPolicy: 'no-cache',
+  });
+
+  const { message, name, role, success, token } = riderCheck ? data.loginRider : data.loginDriver;
+
+  if (success) {
+    localStorage.setItem('token', token);
+    riderCheck ? history.push('/setcourse') : history.push('/signup/select');
+  } else {
+    window.alert(message);
+  }
+};
