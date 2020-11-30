bd9353504f0bbe72858dcb68ae52af71a82bcbb9
passwd10 <parkinseo94@naver.com>
2020-11-30 11:43:52 +0900

    feat: 드라이버 현재위치 지도에 출력
    - circle 이용

--- client/src/components/containers/DriverCurrentPositionMap.tsx
+++ client/src/components/containers/DriverCurrentPositionMap.tsx
@@ -1,16 +1,30 @@
 import React, { useState, useCallback } from 'react';
 
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Circle } from '@react-google-maps/api';
 
 const containerStyle = {
   width: '100%',
-  height: '380px',
+  height: '600px',
 };
 
 const INIT_POS = {
   lat: 37.55,
   lng: 126.97,
 };
+
+const driverPositionOption = {
+  strokeColor: '#FF0000',
+  strokeOpacity: 1,
+  strokeWeight: 2,
+  fillColor: '#FF0000',
+  fillOpacity: 1,
+  clickable: false,
+  draggable: false,
+  editable: false,
+  visible: true,
+  zIndex: 1,
+};
+
 
 function DriverCurrentPositionMap() {
   const [map, setMap] = useState(null);
@@ -24,16 +38,41 @@ function DriverCurrentPositionMap() {
     setMap(null);
   }, []);
 
+  const makeStartingPointHere = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: Position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          setCenter(pos);
+        },
+        () => {
+          console.log('Error: The Geolocation service failed.');
+        },
+      );
+    } else {
+      console.log('Error: Your browser doesn\'t support geolocation');
+    }
+  };
+
+  makeStartingPointHere();
+
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
       <GoogleMap
         mapContainerStyle={containerStyle}
-        zoom={14}
+        zoom={16}
         onLoad={onLoad}
         onUnmount={onUnmount}
         center={center}
       >
-
+        <Circle
+          center={center}
+          radius={10}
+          options={driverPositionOption}
+        />
       </GoogleMap>
     </LoadScript>
   );
