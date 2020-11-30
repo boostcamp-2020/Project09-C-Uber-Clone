1078da1517ff8bff45e9f4d51bba5fd40093944d
Heonjun LEE <dklee730@gmail.com>
2020-11-30 14:44:55 +0900

    chore: 주석삭제

--- server/src/app.ts
+++ server/src/app.ts
@@ -59,7 +59,3 @@ httpServer.listen(process.env.PORT, () => {
   console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
   console.log(`🚀 Subscriptions ready at ws://localhost:${process.env.PORT}${server.subscriptionsPath}`)
 })
-// app.listen({ port: process.env.PORT }, () =>
-//   console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`),
-// );
-
