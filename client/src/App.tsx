f93357a476a065787f998fd8346f361ab103d21c
passwd10 <parkinseo94@naver.com>
2020-11-30 20:18:29 +0900

    feat: 사용자와 관련된 페이지만 Routing
    - driver는 rider페이지에 접근할 수 없음
    - rider는 driver페이지에 접근할 수 없음
    - token이 없으면 login페이지로

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,9 @@ import React from 'react';
 
 import { Route, Switch } from 'react-router-dom';
 
-import LoginPage from './pages/LoginPage';
 import SignUpSelectPage from './pages/SignUpSelectPage';
 import RiderSignUpPage from './pages/RiderSignUpPage';
 import DriverSignUpPage from './pages/DriverSignUpPage';
-import DriverWaitingPage from './pages/DriverWaitingPage';
 
 import RouteIf from './routes/RouteIf';
 
@@ -16,11 +14,9 @@ export default function App() {
   return (
     <>
       <Switch>
-        <Route path='/driver/main' component={DriverWaitingPage} />
         <Route path='/signup/select' component={SignUpSelectPage} />
         <Route path='/signup/rider' component={RiderSignUpPage} />
         <Route path='/signup/driver' component={DriverSignUpPage} />
-        <Route path='/login' component={LoginPage} />
         <RouteIf path='*' />
       </Switch>
     </>
