3060e97356dc21dc3742e2f6af1a7da97a3376a7
ahrimy <dlsdjdkfla@gmail.com>
2020-12-01 23:25:15 +0900

    feat:svg 컴포넌트 적용 및 로그인대상 구분
    - 자동차, 현재위치, 핀 을 나타내는 svg 컴포넌트 적용
    - Rider일 경우 현재위치 아이콘 생성
    - Driver일 경우 자동차 위치를 현재 위치로 업데이트

--- client/src/components/containers/PickUpMap.tsx
+++ client/src/components/containers/PickUpMap.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useCallback, useEffect } from 'react';
 
-import { GoogleMap, Circle, OverlayView } from '@react-google-maps/api';
+import { GoogleMap, OverlayView } from '@react-google-maps/api';
 
-import styled from 'styled-components';
-
-import carImg from '../../../assets/car.png';
-import currentLocationImg from '../../../assets/current-location.png';
+import CarIcon from '../presentational/CarIcon';
+import CurrentLocationIcon from '../presentational/CurrentLocationIcon';
+import PinIcon from '../presentational/PinIcon';
 
 const containerStyle = {
   width: '100%',
@@ -13,40 +12,25 @@ const containerStyle = {
 };
 
 const INIT_POS = {
-  lat: 37.55,
-  lng: 126.97,
+  lat: 34.040,
+  lng: -118.246,
 };
 
-const Car = styled.img`
-  position: absolute;
-  width: 40px;
-  z-index: 100;
-`;
-
-const CurrentLocation = styled.img`
-  position: absolute;
-  width: 40px;
-  z-index: 100;
-`;
-
-const riderPositionOption = {
-  strokeColor: '#56A902',
-  strokeOpacity: 0.8,
-  strokeWeight: 2,
-  fillColor: '#56A902',
-  fillOpacity: 0.35,
-  clickable: false,
-  draggable: false,
-  editable: false,
-  visible: true,
-  zIndex: 1,
+const INIT_DRIVER_POS = {
+  lat: 34.047,
+  lng: -118.249,
 };
 
-function PickUpMap({ isDriver }:{isDriver:boolean}) {
+const INIT_PICKUP_POS = {
+  lat: 34.040,
+  lng: -118.246,
+};
+
+function PickUpMap({ isRider }:{isRider:boolean}) {
   const [map, setMap] = useState(null);
   const [center, setCenter] = useState(INIT_POS);
-  const [userPos, setUserPos] = useState(INIT_POS);
-  const [matcherUserPos, setMatchedUserPos] = useState(INIT_POS);
+  const [driverPos, setDriverPos] = useState(INIT_DRIVER_POS);
+  const [pickUpPos, setPickUpPos] = useState(INIT_PICKUP_POS);
 
   const onLoad = useCallback((map) => {
     setMap(map);
@@ -61,8 +45,11 @@ function PickUpMap({ isDriver }:{isDriver:boolean}) {
       lat: position.coords.latitude,
       lng: position.coords.longitude,
     };
-    if (JSON.stringify(userPos) !== JSON.stringify(pos)) {
-      setUserPos(pos);
+    if (JSON.stringify(center) !== JSON.stringify(pos)) {
+      setCenter(pos);
+      if (!isRider) {
+        setDriverPos(pos);
+      };
     }
   };
 
@@ -91,20 +78,26 @@ function PickUpMap({ isDriver }:{isDriver:boolean}) {
       zoom={16}
       onLoad={onLoad}
       onUnmount={onUnmount}
-      center={userPos}
+      center={center}
     >
       <OverlayView
-        position={isDriver ? userPos : matcherUserPos}
+        position={driverPos}
         mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
       >
-        <Car src={carImg}/>
+        <CarIcon />
       </OverlayView>
       <OverlayView
-        position={isDriver ? matcherUserPos : userPos}
+        position={pickUpPos}
         mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
       >
-        <CurrentLocation src={currentLocationImg}/>
+        <PinIcon />
       </OverlayView>
+      {isRider && <OverlayView
+        position={center}
+        mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+      >
+        <CurrentLocationIcon />
+      </OverlayView>}
     </GoogleMap>
   );
 }
