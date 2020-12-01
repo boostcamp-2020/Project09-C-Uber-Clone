443d6ce52f772ee02af44f166fd01d1c383d249e
Heonjun LEE <dklee730@gmail.com>
2020-12-01 10:48:43 +0900

    feat: client web sockt설정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,6 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
 
 import GlobalStyle from './GlobalStyle';
 import store from './store';
@@ -14,8 +17,30 @@ import App from './App';
 
 const rootElement = document.getElementById('app');
 
+const httpLink = new HttpLink({
+  uri: process.env.REACT_APP_SERVER_URI
+});
+const wsLink = new WebSocketLink({
+  uri: process.env.REACT_APP_WEBSOCKET_URI,
+  options: {
+    reconnect: true
+  }
+});
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
+);
+
 const client = new ApolloClient({
   uri: process.env.REACT_APP_SERVER_URI,
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
