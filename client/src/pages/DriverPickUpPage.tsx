73c2d899a0908088f0b99c754db45d99882c920e
passwd10 <parkinseo94@naver.com>
2020-12-03 00:08:36 +0900

    feat: Form에 map과 infobox를 삽입

--- client/src/pages/DriverPickUpPage.tsx
+++ client/src/pages/DriverPickUpPage.tsx
@@ -4,14 +4,12 @@ import { LoadScript } from '@react-google-maps/api';
 import { Libraries } from '@react-google-maps/api/dist/utils/make-load-script-url';
 const libraries: Libraries = ['places'];
 
-import PickUpMap from '../components/containers/PickUpMap';
-import RiderInfoBox from '../components/containers/RiderInfoBox';
+import DriverPickUpForm from '../components/containers/DriverPickUpForm';
 
 function DriverPickUpPage() {
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY} libraries={libraries}>
-      <PickUpMap isRider={false}/>
-      <RiderInfoBox />
+      <DriverPickUpForm />
     </LoadScript>
   );
 }
--- client/src/pages/RiderPickUpPage.tsx
+++ client/src/pages/RiderPickUpPage.tsx
@@ -4,14 +4,12 @@ import { LoadScript } from '@react-google-maps/api';
 import { Libraries } from '@react-google-maps/api/dist/utils/make-load-script-url';
 const libraries: Libraries = ['places'];
 
-import PickUpMap from '../components/containers/PickUpMap';
-import DriverInfoBox from '../components/containers/DriverInfoBox';
+import RiderPickUpForm from '../components/containers/RiderPickUpForm';
 
 function RiderPickUpPage() {
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY} libraries={libraries}>
-      <PickUpMap isRider={true}/>
-      <DriverInfoBox />
+      <RiderPickUpForm />
     </LoadScript>
   );
 }
