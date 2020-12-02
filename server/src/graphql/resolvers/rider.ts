27d406445ea0860ce6ca72861411486f7b619981
kjk <kkole3897@naver.com>
2020-12-02 15:41:11 +0900

    feat: 라이더 위치 공유 resolver 추가

--- server/src/graphql/resolvers/rider.ts
+++ server/src/graphql/resolvers/rider.ts
@@ -1,3 +1,5 @@
+import { PubSub } from 'apollo-server-express';
+
 import { Rider } from '../../services';
 
 interface LoginPayload{
@@ -12,6 +14,9 @@ interface createRiderArgs {
   phoneNumber: string;
 }
 
+const pubsub = new PubSub();
+
+const MATCHED_RIDER_STATE = 'MATCHED_RIDER_STATE';
 export default {
   Query: {
     async rider(parent: any, args: { email: string }, context: any, info: any) {
@@ -24,6 +29,14 @@ export default {
     },
     async createRider (parent: any, payload: createRiderArgs, context: any) {
       return await Rider.signup(payload);
+    },
+    async notifyRiderState(parent: any, args: any, context: any) {
+      pubsub.publish(MATCHED_RIDER_STATE, { matchedRiderState: args });
+    },
+  },
+  Subscription: {
+    matchedRiderState: {
+      subscribe: () => pubsub.asyncIterator([MATCHED_RIDER_STATE]),
     },
   },
 };
