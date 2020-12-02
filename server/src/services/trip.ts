d8c2a688595fe40a07b0b4c44974680af7ed56dd
ahrimy <dlsdjdkfla@gmail.com>
2020-12-03 00:13:35 +0900

    feat: driverResponse에 대한 서비스
    - 현재 trip의 상태 확인
    - 현재 trip의 상태가 open 일 경우 matched로 변경 후 rider에게 publish

--- server/src/services/trip.ts
+++ server/src/services/trip.ts
@@ -2,4 +2,19 @@ import bcrypt from 'bcrypt';
 
 import { Trip } from '../repositories';
 
-export default {};
+export default {
+  sendDriverResponse: async (args, context) => {
+    try {
+      const driverID = context.req.user.data._id;
+      const data = await Trip.findOneStatus(args.tripID);
+      if (data?.status === 'open') {
+        Trip.updateStatus(args.tripID, 'matched');
+        context.pubsub.publish('driverResponded', { driverResponded: { driverID, ...args } });
+        return { tripID: args.tripID, riderID: args.riderID, result: 'success' };
+      }
+      return { tripID: args.tripID, riderID: args.riderID, result: 'fail' };
+    } catch (e) {
+      throw e.message;
+    }
+  },
+};
