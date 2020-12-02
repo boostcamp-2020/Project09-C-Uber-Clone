f3ccd476b85e12feaca8e3f45334b7a4505b5f21
passwd10 <parkinseo94@naver.com>
2020-12-03 00:10:16 +0900

    feat: driverPosition을 객체로 mutation하기 위해 type지정

--- client/src/apis/driverAPI.tsx
+++ client/src/apis/driverAPI.tsx
@@ -1,17 +1,25 @@
 import { driverStateNotifyQuery } from '../queries/driver';
 
+interface driverPosition {
+  lat: number
+  lng: number
+}
+
 interface driverStateNotifyProps {
   operationId: String
   driverId: String
   riderId: String
-  driverPosition: String
+  driverPosition: driverPosition
   isDrop: Boolean
 }
 
 export const driverStateNotify = async (client: any, driverState: driverStateNotifyProps) => {
-  const { data } = await client.mutate({
-    mutation: driverStateNotifyQuery,
-    variables: driverState,
-    fetchPolicy: 'no-cache',
-  });
+  try {
+    const { data } = await client.mutate({
+      mutation: driverStateNotifyQuery,
+      variables: driverState,
+      fetchPolicy: 'no-cache',
+    });
+  } catch (error) {
+  }
 };
