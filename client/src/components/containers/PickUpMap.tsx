f28eac730a494b41284cd43e5e73d7fea2b39f3e
passwd10 <parkinseo94@naver.com>
2020-12-03 00:09:34 +0900

    refactor: PickUpMap은 부모컴포넌트로부터 받아온 위치만 출력하도록 변경

--- client/src/components/containers/PickUpMap.tsx
+++ client/src/components/containers/PickUpMap.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { GoogleMap, OverlayView } from '@react-google-maps/api';
 
@@ -11,74 +11,46 @@ const containerStyle = {
   height: '75vh',
 };
 
-const INIT_POS = {
-  lat: 34.040,
-  lng: -118.246,
-};
-
-const INIT_DRIVER_POS = {
-  lat: 34.047,
-  lng: -118.249,
-};
-
-const INIT_PICKUP_POS = {
-  lat: 34.040,
-  lng: -118.246,
-};
-
-function PickUpMap({ isRider }:{isRider:boolean}) {
-  const [map, setMap] = useState(null);
-  const [center, setCenter] = useState(INIT_POS);
-  const [driverPos, setDriverPos] = useState(INIT_DRIVER_POS);
-  const [pickUpPos, setPickUpPos] = useState(INIT_PICKUP_POS);
-
-  const onLoad = useCallback((map) => {
-    setMap(map);
-  }, []);
-
-  const onUnmount = useCallback(() => {
-    setMap(null);
-  }, []);
-
-  const success = (position: Position): any => {
-    const pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude,
-    };
-    if (JSON.stringify(center) !== JSON.stringify(pos)) {
-      setCenter(pos);
-      if (!isRider) {
-        setDriverPos(pos);
-      };
-    }
-  };
-
-  const error = (): any => {
-    console.log('Error: The Geolocation service failed.');
-  };
-
-  const options = {
-    enableHighAccuracy: false,
-    maximumAge: 0,
-  };
-
-  const getUserPosition = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.watchPosition(success, error, options);
-    }
-  };
+export default function PickUpMap({ isRider, riderLat, riderLng, driverLat, driverLng, pickUpLat, pickUpLng }: {isRider: Boolean, riderLat: number, riderLng: number, driverLat: number, driverLng: number, pickUpLat: number, pickUpLng: number}) {
+  const [driverPos, setDriverPos] = useState({
+    lat: driverLat,
+    lng: driverLng,
+  });
+  const [pickUpPos, setPickUpPos] = useState({
+    lat: pickUpLat,
+    lng: pickUpLng,
+  });
+  const [riderPos, setRiderPos] = useState({
+    lat: riderLat,
+    lng: riderLng,
+  });
 
   useEffect(() => {
-    getUserPosition();
-  }, []);
+    setDriverPos({
+      lat: driverLat,
+      lng: driverLng,
+    });
+  }, [driverLat]);
+
+  useEffect(() => {
+    setRiderPos({
+      lat: riderLat,
+      lng: riderLng,
+    });
+  }, [riderLat]);
+
+  useEffect(() => {
+    setPickUpPos({
+      lat: pickUpLat,
+      lng: pickUpLng,
+    });
+  }, [pickUpLat]);
 
   return (
     <GoogleMap
       mapContainerStyle={containerStyle}
       zoom={16}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-      center={center}
+      center={isRider ? riderPos : driverPos}
     >
       <OverlayView
         position={driverPos}
@@ -92,14 +64,12 @@ function PickUpMap({ isRider }:{isRider:boolean}) {
       >
         <PinIcon />
       </OverlayView>
-      {isRider && <OverlayView
-        position={center}
+      <OverlayView
+        position={riderPos}
         mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
       >
         <CurrentLocationIcon />
-      </OverlayView>}
+      </OverlayView>
     </GoogleMap>
   );
 }
-
-export default PickUpMap;
