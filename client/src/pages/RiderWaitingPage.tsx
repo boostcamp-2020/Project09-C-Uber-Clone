4b2cfac0e6a01d900a08f5cd06fb64593b36e297
ahrimy <dlsdjdkfla@gmail.com>
2020-12-03 21:43:39 +0900

    style: 추가 개행 제거

--- client/src/pages/RiderWaitingPage.tsx
+++ client/src/pages/RiderWaitingPage.tsx
@@ -1,10 +1,7 @@
-
 import React from 'react';
 
 import RiderPickupPositionMap from '../components/containers/RiderPickupPositionMap';
 import RiderWaitingModal from '../components/containers/RiderWaitingModal';
-
-
 
 function RiderWaitingPage() {
   return (
