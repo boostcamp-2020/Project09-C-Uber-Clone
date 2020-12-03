a34b8f917fa880e620f6ef6b22ef54022ccea861
passwd10 <parkinseo94@naver.com>
2020-12-03 11:27:32 +0900

    chore: 사용하지 않는 place api 제거

--- client/src/pages/RiderPickUpPage.tsx
+++ client/src/pages/RiderPickUpPage.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 import { LoadScript } from '@react-google-maps/api';
-import { Libraries } from '@react-google-maps/api/dist/utils/make-load-script-url';
-const libraries: Libraries = ['places'];
 
 import RiderPickUpForm from '../components/containers/RiderPickUpForm';
 
 function RiderPickUpPage() {
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY} libraries={libraries}>
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY}>
       <RiderPickUpForm />
     </LoadScript>
   );
