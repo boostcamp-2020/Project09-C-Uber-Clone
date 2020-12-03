8a644f227c44c91426dfaa32daaaf1e6061f3510
ahrimy <dlsdjdkfla@gmail.com>
2020-12-04 02:29:44 +0900

    feat: 드라이버 알림 팝업 로직 추가

--- client/src/components/presentational/DriverPopup.tsx
+++ client/src/components/presentational/DriverPopup.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useApolloClient } from '@apollo/client';
+import { useDispatch } from 'react-redux';
 
 import { Flex } from 'antd-mobile';
 
 import styled from 'styled-components';
 
+import { sendDriverResponse } from '../../apis/driverResponseAPI';
+
 import IgnoreButton from '../presentational/IgnoreButton';
 import SubmitButton from '../presentational/SubmitButton';
+
+import { DRIVER_IGNORED, DRIVER_MATCHING_SUCCESS } from '../../constants/driverStatus';
+import { ALREADY_MATCHED, MATCHING_SUCCESS, MATCHING_CANCEL } from '../../constants/matchingResult';
+
 
 const Modal = styled.div`
   position: absolute;
@@ -44,18 +52,66 @@ const ExpectationHeader = styled.div`
   color: gray;
 `;
 
-function DriverPopup() {
-  //TODO: IGONRE 버튼 클릭 시 다음 호출 대기
-  //TODO: CONFIRM 버튼 클릭 시 driverResponse mutate,
-  //      response의 result가 success 이면 callRequested subscription 취소 후 다음 화면
-  //      response의 result가 fail 이면 다음 호출 대기
+const Alert = styled.div`
+  position: absolute;
+  top: 30%;
+  right: 0;
+  left: 0;
+  width: 360px;
+  height: 180px;
+  margin: auto;
+  padding: 16px 12px;
+  background-color: white;
+  font-size: 30px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 11;
+`;
+
+interface TripObject{
+  id: number
+}
+
+const PAYLOAD = { response: 'confirm', riderId: '5fbf5149f35512f0653ec7f1', tripId: '5fc7a18bab179a163b73302b' };
+function DriverPopup({ trip, setDriverStatus }:{trip:TripObject, setDriverStatus:any}) {
+  const client = useApolloClient();
+  const dispatch = useDispatch();
+  const [status, setStatus] = useState('');
+
+  const showAlert = (result:string) => {
+    setStatus(result);
+    setTimeout(() => {
+      setDriverStatus(DRIVER_IGNORED);
+    }, 2000);
+  };
+
+  const handleClickIgnoreButton = () => {
+    setDriverStatus(DRIVER_IGNORED);
+  };
+
+  const handleClickSubmitButton = async() => {
+    const { result, riderId, tripId } = await sendDriverResponse(client, dispatch, PAYLOAD);
+    if (result === MATCHING_SUCCESS) {
+      return setDriverStatus(DRIVER_MATCHING_SUCCESS);
+    } else {
+      showAlert(result);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDriverStatus(DRIVER_IGNORED);
+    }, 5000);
+  });
   return (
-    <>
-      <ModalOverlay />
+
+    <ModalOverlay >
       <Modal>
         <Flex>
           <Flex.Item>
-            <PlaceHeader>픽업 위치</PlaceHeader>
+            <PlaceHeader>픽업 위치 {trip.id}</PlaceHeader>
           </Flex.Item>
           <Flex.Item>
             <PlaceHeader>도착지 위치</PlaceHeader>
@@ -66,14 +122,17 @@ function DriverPopup() {
         </Expectation>
         <Flex>
           <Flex.Item>
-            <IgnoreButton content='IGNORE' />
+            <IgnoreButton content='IGNORE' onClick={handleClickIgnoreButton} />
           </Flex.Item>
           <Flex.Item>
-            <SubmitButton content='CONFIRM' disabled={false} onClick={false} />
+            <SubmitButton content='CONFIRM' disabled={false} onClick={handleClickSubmitButton} />
           </Flex.Item>
         </Flex>
       </Modal>
-    </>
+      {status === ALREADY_MATCHED && <Alert><div>이미 매칭 완료된 요청</div></Alert>}
+      {status === MATCHING_CANCEL && <Alert ><div>취소된 요청</div></Alert>}
+    </ModalOverlay>
+
   );
 }
 
--- client/src/pages/DriverWaitingPage.tsx
+++ client/src/pages/DriverWaitingPage.tsx
@@ -1,29 +1,59 @@
 import React, { useState, useEffect } from 'react';
-
-import { useSubscription } from '@apollo/client';
+import { useHistory } from 'react-router-dom';
+import { useApolloClient, useSubscription } from '@apollo/client';
 
 import { driverListen } from '../queries/callRequest';
+import { getTripStatus } from '../apis/tripAPI';
 
 import DriverCurrentPositionMap from '../components/containers/DriverCurrentPositionMap';
 import DriverPopup from '../components/presentational/DriverPopup';
+import { DRIVER_MATCHING_SUCCESS, DRIVER_POPUP, DRIVER_IGNORED, DRIVER_WAITING } from '../constants/driverStatus';
 
 function DriverWaitingPage() {
+  //TODO: 이 페이지 전체를 container로 이동
+  const client = useApolloClient();
+  const history = useHistory();
   const { loading, error, data } = useSubscription(driverListen);
   const [riderCalls, setRiderCalls] = useState([]);
-
-  useEffect(() => {
-    if (data) {
-      setRiderCalls([...riderCalls, data]);
-    }
-  }, [data]);
+  const [trip, setTrip] = useState({ id: undefined }); //TODO: type 다시 지정
+  const [driverStatus, setDriverStatus] = useState(DRIVER_WAITING);
 
   if (error) {
     console.log(error);
   }
 
+  useEffect(() => {
+    if (data && driverStatus !== DRIVER_MATCHING_SUCCESS) {
+      setRiderCalls([...riderCalls, data]);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (driverStatus === DRIVER_WAITING && riderCalls[0]) {
+      setTrip(riderCalls[0]);
+    }
+  }, [riderCalls]);
+
+  useEffect(() => {
+    //TODO: trip 의 id
+    getTripStatus(client, { id: '5fc7a18bab179a163b73302b' }, setDriverStatus);
+  }, [trip]);
+
+  useEffect(() => {
+    if (driverStatus === DRIVER_IGNORED) {
+      setDriverStatus(DRIVER_WAITING);
+      setRiderCalls(riderCalls.slice(1));
+    }
+    if (driverStatus === DRIVER_MATCHING_SUCCESS) {
+      setRiderCalls([]);
+      //TODO: trip 정보 전역으로 저장
+      history.push('/driver/pickup');
+    }
+  }, [driverStatus]);
+
   return (
     <>
-      {riderCalls.length > 0 && <DriverPopup />}
+      {driverStatus === DRIVER_POPUP && <DriverPopup trip={trip} setDriverStatus={setDriverStatus}/>}
       <DriverCurrentPositionMap />
     </>
   );
