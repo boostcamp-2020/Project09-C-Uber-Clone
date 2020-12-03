0f72f31b54995bc638163f665539135f59e081c5
kjk <kkole3897@naver.com>
2020-12-04 01:29:17 +0900

    refactor: reverseGeocoding util로 모듈화
    - 좌표로부터 주소를 가져오는 reverseGeocoding을 여러 파일에서 사용하게 되어 모듈화

---
+++ client/src/utils/geocoding.ts
@@ -0,0 +1,17 @@
+export const reverseGoecoding = (position: {lat: number, lng: number}): Promise<string> => {
+  const geocoder = new google.maps.Geocoder;
+  return new Promise((resolve, reject) => {
+    geocoder.geocode(
+      { location: position },
+      (
+        results: google.maps.GeocoderResult[],
+        status: google.maps.GeocoderStatus,
+      ) => {
+        if (status === 'OK' && results[0]) {
+          resolve(results[0].formatted_address);
+        }
+        reject(new Error('cannot find address'));
+      },
+    );
+  });
+};
