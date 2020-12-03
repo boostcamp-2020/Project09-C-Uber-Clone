981d066e21cb23d1ddd91ac1fd5673b1492c7b98
Heonjun LEE <dklee730@gmail.com>
2020-12-03 18:49:38 +0900

    fix: 타입에러 수정, 예외처리

--- client/src/apis/callRequestAPI.tsx
+++ client/src/apis/callRequestAPI.tsx
@@ -1,13 +1,22 @@
 import { driverCall } from '../queries/callRequest';
 
-export const callRequest = async (client: any, driverIds: any, riderId: any, origin : any, destination: any) => {
-  const { data } = await client.mutate({
-    mutation: driverCall,
-    variables: { driverIds, riderId, origin, destination },
-    fetchPolicy: 'no-cache',
-  });
+import { ApolloClient } from '@apollo/client';
+
 interface Position {
   lat: number;
   lng: number;
 }
+
+export const callRequest = async (client: ApolloClient<Object>, driverIds: string[], riderId: string, origin : Position, destination: Position) => {
+  try {
+    await client.mutate({
+      mutation: driverCall,
+      variables: { driverIds, riderId, origin, destination },
+      fetchPolicy: 'no-cache',
+    });
+    window.alert('경로 전송');
+  } catch (error) {
+    window.alert('실패');
+    console.log(error);
+  }
 };
