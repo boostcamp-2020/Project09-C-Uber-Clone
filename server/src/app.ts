b23387a7f1525631c20606eabae6dbef94e7dc3f
ahrimy <dlsdjdkfla@gmail.com>
2020-12-03 12:10:36 +0900

    chore: mongoose 옵션 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,6 +16,7 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
+  useFindAndModify: false,
 };
 mongoose.connect(process.env.MONGODB_URL || '', options)
   .then(() => console.log('Successfully connected to mongodb'))
