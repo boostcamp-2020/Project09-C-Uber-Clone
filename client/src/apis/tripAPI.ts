58fd30a4314d547cfcbcaf499b55918624e2b0c1
ahrimy <dlsdjdkfla@gmail.com>
2020-12-04 02:17:52 +0900

    feat: driverResponseAPI 및 tripAPI 로직 추가

--- client/src/apis/tripAPI.ts
+++ client/src/apis/tripAPI.ts
@@ -2,14 +2,22 @@ import { ApolloClient } from '@apollo/client';
 
 import { getStatus } from '../queries/trip';
 
-export const getTripStatus = async (client: ApolloClient<Object>, tripInfo:{id:string}) => {
+import { OPEN } from '../constants/tripStatus';
+import { DRIVER_POPUP, DRIVER_IGNORED } from '../constants/driverStatus';
+
+export const getTripStatus = async (client: ApolloClient<Object>, tripInfo:{id:string}, setDriverStatus:any) => {
   try {
-    const { data } = await client.query({
+    const { data: { tripStatus } } = await client.query({
       query: getStatus,
       variables: tripInfo,
       fetchPolicy: 'no-cache',
     });
-    console.log(data);
+    console.log(tripStatus);
+    if (tripStatus === OPEN) {
+      setDriverStatus(DRIVER_POPUP);
+    } else {
+      setDriverStatus(DRIVER_IGNORED);
+    }
   } catch (error) {
   }
 };
