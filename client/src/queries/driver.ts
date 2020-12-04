00a67baaedaf6938a015d8ed0bc81c3e52f01777
ahrimy <dlsdjdkfla@gmail.com>
2020-12-04 11:45:54 +0900

    fix: 쿼리 및 타입 정의 수정

--- client/src/queries/driver.ts
+++ client/src/queries/driver.ts
@@ -3,20 +3,14 @@ import { gql } from '@apollo/client';
 export const driverStateNotifyQuery = gql`
 mutation driverStateNotify(
   $tripId:String,
-  $driverId:String,
-  $riderId:String,
   $driverPosition:DriverPositionInput,
   $isDrop:Boolean){
     driverStateNotify(
       tripId:$tripId,
-      driverId:$driverId,
-      riderId:$riderId,
       driverPosition:$driverPosition,
       isDrop:$isDrop
     ){
       tripId
-      driverId
-      riderId
       isDrop
       driverPosition {
         lat
