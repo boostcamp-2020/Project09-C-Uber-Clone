0ee6e9a6bf0bab47d0b5f5a6b53c12c9d26159c7
passwd10 <parkinseo94@naver.com>
2020-12-06 20:23:34 +0900

    feat: 드라이버 대기페이지에서 1초마다 드라이버 위치 업데이트하도록 구현

--- client/src/pages/DriverWaitingPage.tsx
+++ client/src/pages/DriverWaitingPage.tsx
@@ -3,26 +3,53 @@ import { useHistory } from 'react-router-dom';
 import { useApolloClient, useSubscription } from '@apollo/client';
 
 import { driverListen } from '../queries/callRequest';
+
 import { getTripStatus } from '../apis/tripAPI';
+import { updateDriverPosition } from '../apis/driverAPI';
 
 import DriverCurrentPositionMap from '../components/containers/DriverCurrentPositionMap';
 import DriverPopup from '../components/presentational/DriverPopup';
 import { DRIVER_MATCHING_SUCCESS, DRIVER_POPUP, DRIVER_IGNORED, DRIVER_WAITING } from '../constants/driverStatus';
 
+const DRIVER_POSITION_UPDATE_TIME = 1000;
+
 function DriverWaitingPage() {
   //TODO: 이 페이지 전체를 container로 이동
   const client = useApolloClient();
   const history = useHistory();
-  const { loading, error, data } = useSubscription(driverListen);
+  const { data } = useSubscription(driverListen);
   const [riderCalls, setRiderCalls] = useState([]);
   const [trip, setTrip] = useState({ id: undefined }); //TODO: type 다시 지정
   const [driverStatus, setDriverStatus] = useState(DRIVER_WAITING);
   const [pickUpAddress, setPickUpAddress] = useState('');
   const [destinationAddress, setDestinationAddress] = useState('');
+  const [count, setCount] = useState(0);
+  const [driverPos, setDriverPos] = useState({ lat: 0, lng: 0 });
 
-  if (error) {
-    console.log(error);
-  }
+  const success = (position: Position): any => {
+    const pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+    if (JSON.stringify(driverPos) !== JSON.stringify(pos)) {
+      setDriverPos(pos);
+    }
+  };
+
+  const navError = (): any => {
+    console.log('Error: The Geolocation service failed.');
+  };
+
+  const options = {
+    enableHighAccuracy: false,
+    maximumAge: 0,
+  };
+
+  const getDriverPosition = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, navError, options);
+    }
+  };
 
   useEffect(() => {
     if (data && driverStatus !== DRIVER_MATCHING_SUCCESS) {
@@ -56,6 +83,18 @@ function DriverWaitingPage() {
     }
   }, [driverStatus]);
 
+  useEffect(() => {
+    getDriverPosition();
+    setTimeout(() => {
+      setCount(count + 1);
+      updateDriverPosition(client, driverPos);
+    }, DRIVER_POSITION_UPDATE_TIME);
+  }, [count]);
+
+  useEffect(() => {
+    getDriverPosition();
+  }, []);
+
   return (
     <>
       {driverStatus === DRIVER_POPUP &&
@@ -66,7 +105,7 @@ function DriverWaitingPage() {
         destinationAddress={destinationAddress}
         setDriverStatus={setDriverStatus}
       />}
-      <DriverCurrentPositionMap />
+      <DriverCurrentPositionMap driverPos={driverPos}/>
     </>
   );
 }
