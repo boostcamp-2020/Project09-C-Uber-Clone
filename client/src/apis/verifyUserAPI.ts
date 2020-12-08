201f965afa07637308ce5ff51391569080f05ef1
ahrimy <dlsdjdkfla@gmail.com>
2020-12-08 10:42:27 +0900

    refactor: 변경된 쿼리 API에 적용

--- client/src/apis/verifyUserAPI.ts
+++ client/src/apis/verifyUserAPI.ts
@@ -1,12 +1,12 @@
 import { ApolloClient } from '@apollo/client';
 
-import { verifyQuery } from '../queries/verify';
+import { USER_ROLE } from '../queries/verify';
 
 import { setLoginRole } from '../slices/loginSlice';
 
 export const verifyUser = async (client: ApolloClient<object>, dispatch: any) => {
   const { data } = await client.mutate({
-    mutation: verifyQuery,
+    mutation: USER_ROLE,
     fetchPolicy: 'no-cache',
   });
 
