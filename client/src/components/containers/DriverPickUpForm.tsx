ab0b4ccf4afdff3f52df7961382082c3fe41a946
ahrimy <dlsdjdkfla@gmail.com>
2020-12-08 17:16:34 +0900

    fix: 빈문자열이 tripid에 저장되는 오류 수정

--- client/src/components/containers/DriverPickUpForm.tsx
+++ client/src/components/containers/DriverPickUpForm.tsx
@@ -72,10 +72,6 @@ export default function DriverPickUpForm() {
     notifyDriverState({ variables: { tripId: trip.id, driverPosition: driverPos, isDrop: false } });
   }, [driverPos]);
 
-  useEffect(() => {
-    localStorage.setItem('tripId', trip.id);
-  }, []);
-
   if (error) {
     return <p>error</p>;
   }
