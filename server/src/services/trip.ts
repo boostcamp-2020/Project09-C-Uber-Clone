e5834fe4e5e449e0d26ff15e81cfd431b1d76cb2
kjk <kkole3897@naver.com>
2020-12-08 17:53:04 +0900

    fix: return문 추가
    - return이 없어서 아무런 데이터도 반환하지 않아서 수정

--- server/src/services/trip.ts
+++ server/src/services/trip.ts
@@ -85,10 +85,10 @@ export default {
       throw e;
     }
   },
-  getChattings: async (tripId) => {
-    Trip.getChattings(tripId);
+  getChattings: async (tripId: string) => {
+    return await Trip.getChattings(tripId);
   },
   addChatting: async (tripId: string, chatting: {text: string, time: Date, userId: string}) => {
-    Trip.addChatting(tripId, chatting);
+    return await Trip.addChatting(tripId, chatting);
   },
 };
