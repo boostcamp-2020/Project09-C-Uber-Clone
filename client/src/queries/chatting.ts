dc8bf20f3280a06eea6a2f80d1b5bbe3000f26a6
kjk <kkole3897@naver.com>
2020-12-08 18:32:30 +0900

    feat: ADD_CHATTING 정의
    - 새로운 채팅을 추가하는 요청 정의

--- client/src/queries/chatting.ts
+++ client/src/queries/chatting.ts
@@ -10,3 +10,14 @@ export const GET_CHATTINGS = gql`
     }
   }
 `;
+
+export const ADD_CHATTING = gql`
+  mutation addChatting($tripId: ID!, $chatting: ChattingInput!) {
+    addChatting(tripId: $tripId, chatting: $chatting) {
+      id
+      text
+      time
+      userId
+    }
+  }
+`;
