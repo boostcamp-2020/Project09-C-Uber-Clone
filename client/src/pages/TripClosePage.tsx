12e9e452d069649847a8123a7bc3bf84412d8960
Heonjun LEE <dklee730@gmail.com>
2020-12-10 21:44:52 +0900

    style: 하차정보 화면 레이아웃 수정

--- client/src/pages/TripClosePage.tsx
+++ client/src/pages/TripClosePage.tsx
@@ -5,17 +5,16 @@ import { useSelector } from 'react-redux';
 
 import { useQuery } from '@apollo/client';
 
-import { WhiteSpace, Icon, Flex } from 'antd-mobile';
+import { WhiteSpace, Icon, Steps } from 'antd-mobile';
 
 import styled from 'styled-components';
 
 import { GET_TRIP } from '../queries/trip';
 
 const Page = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  position: absolute;
+  left: 30px;
+  top: 50px;
 `;
 
 const Paper = styled.div`
@@ -23,15 +22,19 @@ const Paper = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 80%;
-  height : 70%;
-  box-shadow: 5px 5px 5px wheat;
+  width:90%;
+  height: 692px;
+  box-shadow: 5px 1px 10px 2px grey;
+  padding: 10%;
+  padding-top: 17%;
+  padding-bottom: 17%;
+  bottom: 30%;
 `;
 
 const Button = styled.button`
   background-color: #56A902;
-  width: 329px;
-  height: 180px;
+  width: 100px;
+  height: 60px;
   border: none;
   color: #fff;
   font-size: 24px;
@@ -41,8 +44,6 @@ const Button = styled.button`
 `;
 
 const Title = styled.h1`
-  width: 89px;
-  height: 29px;
   left: calc(50% - 89px/2 - 2.5px);
   top: calc(50% - 29px/2 - 329.5px);
 
@@ -50,13 +51,8 @@ const Title = styled.h1`
   font-weight: bold;
   font-size: 24px;
   line-height: 29px;
-  margin-bottom: 120px;
-`;
-
-const PlaceHeader = styled.div`
-  text-align: center;
-  font-size: 18px;
-  font-weight: bold;
+  margin-bottom: 50px;
+  margin-top: 30px;
 `;
 
 function TripClosePage() {
@@ -66,8 +62,16 @@ function TripClosePage() {
   const { loginRole } = loginReducer;
   const nextPage = loginRole === 'rider' ? '/rider/setcourse' : '/driver/main';
 
-  const { loading, data: tripData } = useQuery(GET_TRIP, { variables: { id: trip.id } });
+  const { loading, data: tripData } = useQuery(GET_TRIP, { variables: { id: trip.id }, fetchPolicy: 'no-cache' });
 
+  const customIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 42 42" className="am-icon am-icon-md">
+      <g fillRule="evenodd" stroke="transparent" strokeWidth="4">
+        <path d="M21 0C9.402 0 0 9.402 0 21c0 11.6 9.402 21 21 21s21-9.4 21-21C42 9.402 32.598 0 21 0z" />
+        <path fill="#FFF" d="M29 18.73c0-.55-.447-1-1-1H23.36l4.428-5.05c.407-.46.407-1.208 0-1.668-.407-.46-1.068-.46-1.476 0l-5.21 5.89-5.21-5.89c-.406-.46-1.067-.46-1.475 0-.406.46-.406 1.207 0 1.667l4.43 5.05H14.23c-.55 0-.998.45-.998 1 0 .554.448.97 1 .97h5.9v3.942h-5.9c-.552 0-1 .448-1 1s.448.985 1 .985h5.9v4.896c0 .552.448 1 1 1 .55 0 .968-.284.968-.836v-5.06H28c.553 0 1-.433 1-.985s-.447-1-1-1h-5.9v-3.94H28c.553 0 1-.418 1-.97z" />
+      </g>
+    </svg>
+  );
   useEffect(() => {
     if (tripData) {
       setTripInfo(tripData.trip);
@@ -81,14 +85,11 @@ function TripClosePage() {
           <Icon type="check-circle" size='lg' color='#56A902'/>
           <Title>your trip has ended</Title>
           <WhiteSpace />
-          <Flex>
-            <Flex.Item>
-              <PlaceHeader>픽업 위치 : {tripInfo && tripInfo.origin.address}</PlaceHeader>
-            </Flex.Item>
-            <Flex.Item>
-              <PlaceHeader>도착지 위치 : {tripInfo && tripInfo.destination.address}</PlaceHeader>
-            </Flex.Item>
-          </Flex>
+          <Steps current={1}>
+            <Steps.Step title="승차지" icon={customIcon()} description={tripInfo && tripInfo.origin.address} />
+            <Steps.Step title="하차지" icon={customIcon()} description={tripInfo && tripInfo.destination.address} />
+          </Steps>
+          <WhiteSpace />
           <Link to={nextPage}>
             <Button>확인</Button>
           </Link>
