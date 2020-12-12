05ddcdea18ed784237115e756c177acf121c4832
passwd10 <parkinseo94@naver.com>
2020-12-12 23:34:58 +0900

    feat: 사용자 경로설정페이지에 적용

--- client/src/pages/SetCoursePage.tsx
+++ client/src/pages/SetCoursePage.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
 import { LoadScript } from '@react-google-maps/api';
+import { Libraries } from '@react-google-maps/api/dist/utils/make-load-script-url';
 
 import SetCourseForm from '../components/containers/SetCourseForm';
-import { Libraries } from '@react-google-maps/api/dist/utils/make-load-script-url';
+import NoticeModal from '../components/presentational/NoticeModal';
 
 const libraries: Libraries = ['places'];
 
 function SetCoursePage() {
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY} libraries={libraries}>
-      <SetCourseForm />
-    </LoadScript>
+    <>
+      <NoticeModal />
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_API_KEY} libraries={libraries}>
+        <SetCourseForm />
+      </LoadScript>
+    </>
   );
 }
 
